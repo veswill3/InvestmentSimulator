22bb9b61019e9f25b685f2272a5d3345f162af5e
Vesper Williams <veswill3@gmail.com>
2013-05-25 17:47:52 -0500

    can specify chunk size

--- chunk_daily_data.m
+++ chunk_daily_data.m
@@ -1,17 +1,28 @@
 function [ time, price, quantity ] ...
-    = chunk_daily_data( TimeData, PriceData, QuantityData )
+    = chunk_daily_data( TimeData, PriceData, QuantityData, secPerChunk )
 % CHUNK_DAILY_DATA peice out daily stock data for plotting and analysis
 % Created by Vesper
 %   Peice out daily stock data into usable chunks for plotting and analysis
 %   TimeData, PriceData, and QuantityData are vectors returned from
 %   daily_stock_data.
+%   secPerChunk defines the number of seconds to use per chunk. The default
+%   will be the min possible chunk size (1 second?)
 %
 %   time is returned as the unique values from Time in the specified chunks
 %   price is returned as the average or mean of prices within each chunk
 %   quantity is returned as the sum of quantities within each chunk
 
-    % get the unique time values
-    time = unique(TimeData);
+    if nargin < 3
+        error('You must provide TimeData, PriceData, and QuantityData');
+    end
+    if exist('secPerChunk', 'var')
+        oneSec = (1/86400);
+        time = (min(TimeData):secPerChunk*oneSec:max(TimeData))';
+    else
+        % get the unique time values
+        time = unique(TimeData);
+    end
+
     [~, tb] = histc(TimeData, time);
     PriceSparse = sparse(1:numel(TimeData), tb, PriceData);
     % we want the mean of prices for each time
