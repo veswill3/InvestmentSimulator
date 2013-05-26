4d83dcf57c1f9df27bda1567665dc28bf0c79521
Vesper Williams <veswill3@gmail.com>
2013-05-26 13:12:06 -0500

    create folder if it does not exist

--- save_daily_stock_data.m
+++ save_daily_stock_data.m
@@ -4,13 +4,19 @@ function save_daily_stock_data( ticker )
 %   Intraday_data folder in the current working directory. The filename
 %   will be date_ticker.mat where date is of the format yyyymmdd
 
+    SAVE_FOLDER = 'Intraday_data';
+    % make sure folder exists
+    if exist(SAVE_FOLDER, 'file') ~= 7
+        mkdir(SAVE_FOLDER); % create the folder
+    end
+
     % get most recent stock data
     stockData = daily_stock_data(ticker);
 
     % generate filepath (date-ticker)
     dataStr = datestr(stockData(1).Time(1), 'yyyymmdd');
     filename = strcat(dataStr, '_', ticker);
-    path = fullfile(pwd, 'Intraday_data', filename);
+    path = fullfile(pwd, SAVE_FOLDER, filename);
 
     % save to a .mat file
     save(path, 'stockData');
