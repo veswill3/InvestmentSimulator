e7367a8baee9967c4d6880cef15fb1e2bf8c5044
heiden12 <alexheiden114@gmail.com>
2013-06-08 20:41:16 -0400

    New script to connect to the mariaDB

---
+++ database_connection_test.m
@@ -0,0 +1,17 @@
+%This script sets up the DB connection
+%
+%This script is intended to be used to connect to remote
+%a MySQL/Maria database using a JDBC connection.
+%
+%Created by: Alex Heiden
+%Created on: 2013-06-08
+
+dbname = 'stockdata_t01';
+username = 'alex';
+password = '';
+driver = 'com.mysql.jdbc.Driver';
+dburl = ['jdbc:mysql://10.0.0.15:3306/' dbname];
+
+javaclasspath('/home/alex/Projects/DB/mysql-connector-java-5.1.25/mysql-connector-java-5.1.25-bin.jar');
+
+conn = database(dbname, username, password, driver, dburl);
