dae7babbfb39fbbe02800f92131e3f42945c7981
heiden12 <alexheiden114@gmail.com>
2013-07-07 16:07:21 -0400

    Changes to MACD, WillpctR, and adosc.

--- test_WillpctR.m
+++ test_WillpctR.m
@@ -1,9 +1,15 @@
+% Gather stock data
 stock = hist_stock_data('01012013','06282013','RHT')
 
+% Calculate Williams %R for 14 periods (days)
 wpctr = willpctr(stock.High,stock.Low,stock.Close,14)
 
-wpctrSignal = zeros(size(wpctr));
-wpctrSignal(wpctr<-80) = 1
+% Determine the signal, buy or sell, based on Williams %R
+wpctrSignal = ones(size(wpctr));
+wpctrSignal(wpctr>-20) = -1
 
+% Plot the closing price of the stock
 subplot(2,1,1), plot(stock.Close)
+
+% Plot Williams %R
 subplot(2,1,2), plot(wpctr)
--- test_adosc.m
+++ test_adosc.m
@@ -1,9 +1,27 @@
+% % Gather stock data
 stock = hist_stock_data('01012013','06282013','RHT')
 
+% Calculate the Accumulation/Distribution Oscillator
 ado = adosc(stock.High, stock.Low, stock.Open, stock.Close);
+
+% Calculate the Accumulation/Distribution Line
 adl = adline(stock.High, stock.Low, stock.Close, stock.Volume);
 
-adoscSignal = zeros(size(ado));
+% Determine the signal, buy or sell, based on the Accumulation/Distribution
+% Line
+adoscSignal = {''};
+tic
+for i = 1:(size(adl) - 1)
+    if adl(i) < adl(i + 1) && stock.Close(i) > stock.Close(i + 1)
+        adoscSignal{i} = 1;
+        
+    elseif adl(i) > adl(i + 1) && stock.Close(i) < stock.Close( i + 1 )
+        adoscSignal{i} = -1;
+    else
+        adoscSignal{i} = 0;
+    end
+end      
+toc
 
 subplot(3,1,1), plot(stock.Close)
 subplot(3,1,2), plot(adl)
