b93713bf91f1bba3ae93ef2227d56a7116f62fd9
Vesper Williams <veswill3@gmail.com>
2013-07-10 00:05:19 -0500

    start putting signals together

--- START_HERE_RunSimulation.m
+++ START_HERE_RunSimulation.m
@@ -9,7 +9,7 @@ fprintf('Downloading stock data... ');
 tic
 % for testing
 data = hist_stock_data(datestr(startDate, 'ddmmyyyy'), ...
-    datestr(endDate, 'ddmmyyyy'), 'NUAN', 'NVDA', 'ORCL', 'ORLY');
+    datestr(endDate, 'ddmmyyyy'), 'GOOG');
 
 % data = hist_stock_data(datestr(endDate, 'ddmmyyyy'), ...
 %     datestr(startDate, 'ddmmyyyy'), 'Nasdaq.txt');
@@ -20,15 +20,34 @@ addpath('signal_generators/')
 fprintf('Generating signals...\n')
 for i = 1:length(data)
     stock = data(i);
+    
+    signal = stock.Date;
     fprintf('%s', stock.Ticker)
-    signal_MACD = MACD_signal(stock);
+    signal = [signal, MACD_signal(stock)];
     fprintf(' [Press enter to continue]\n'), pause
-    signal_WillPctR = WillPctR_signal(stock);
+    signal = [signal, WillPctR_signal(stock)];
     fprintf(' [Press enter to continue]\n'), pause
-    signal_ADOSC = adosc_signal(stock);
+    signal = [signal, adosc_signal(stock)];
     fprintf(' [Press enter to continue]\n'), pause
-    signal_Stochosc = Stochosc_signal(stock);
+    signal = [signal, Stochosc_signal(stock)];
     fprintf(' [Press enter to continue]\n'), pause
+    
+    Close = stock.Close;
+    Volume = stock.Volume;
+    Date = stock.Date;
+
+    h1 = subplot(8,1, [1;3]); stairs(Close); ylabel('Close');
+    title(h1, stock.Ticker);
+    grid(h1, 'on');
+    h2 = subplot(8,1,4); stem(Volume,'Marker','none'); ylabel('Volume');
+    h3 = subplot(8,1,5); bar(signal(:,2)'); ylabel('MACD');
+    h4 = subplot(8,1,6); bar(signal(:,3)'); ylabel('W%R');
+    h5 = subplot(8,1,7); bar(signal(:,4)'); ylabel('ADOSC');
+    h6 = subplot(8,1,8); bar(signal(:,5)'); ylabel('STO');
+    set([h1, h2, h3, h4, h5], 'XTick', []); % remove x axis from all but bottom
+    linkaxes([h1, h2, h3, h4, h5, h6], 'x'); % keep plots aligned along x
+    datetick(h6, 'keeplimits'); % show dates instead of datenum
+    pause; fprintf('[Press enter to continue]\n');
 end
 fprintf('Done generating signals\n')
 %rmpath('signal_generators/')
